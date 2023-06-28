4fb51312d5975819c9eb68f0e50704d322ac7503
Lindsay <lshank017@gmail.com>
2023-06-27 21:13:49 -0500

    added typerwriter JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+    // fullpage js
+    $('#fullpage').fullpage({
+    licenseKey: 'gplv3-license',
+    navigation: true,
+    navigationTooltips: ["Welcome", "New Robots", "Used Robots", "Contact"] 
+    });
+
+    //typewriterjs
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+    console.log(typewriterElements)
+    for (var i = 0; i < typewriterElements.length; i++){
+        var currentEl = typewriterElements[i]
+        var innerText = currentEl.innerText
+
+        new Typewriter(currentEl, {
+            loop: true,
+        })
+
+        .typeString(innerText)
+        .pauseFor(820)
+        .start()
+    }
+});
